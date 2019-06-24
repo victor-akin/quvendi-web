2a2d09f30d6d601d42671cf8ae7fe5fc8de1ca9e
victor akinyokun <vicgbadebo@gmail.com>
2019-06-24 17:35:41 +0100

    log update

--- config/logging.php
+++ config/logging.php
@@ -42,7 +42,7 @@ return [
 
         'single' => [
             // 'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            // 'path' => storage_path('logs/laravel.log'),
             'driver' => 'errorlog',
             'level' => 'debug',
         ],
