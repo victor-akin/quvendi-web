a4fa8cba72ad61807fd110f5cf72ee69527a2f63
victor akinyokun <vicgbadebo@gmail.com>
2019-06-27 12:46:06 +0100

    env files

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -16,7 +16,7 @@ class TrustProxies extends Middleware
 
     /**
      * The headers that should be used to detect proxies.
-     *
+     *  
      * @var int
      */
     // protected $headers = Request::HEADER_X_FORWARDED_ALL;
