e6fdf91561f920ba8aa3e13f669357cf13d567e7
victor akinyokun <vicgbadebo@gmail.com>
2019-08-09 07:12:00 +0100

    assets for heroku

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -6,9 +6,11 @@
                     <div class="logo-icon-container">
                         <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
                         @if($admin_logo_img == '')
-                            <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
+                            {{-- <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon"> --}}
+                            <img src="{{ url('https://drive.google.com/uc?id=119tvwRPZ9mAC3qRfVy8-Sze1ixsdltNB') }}" alt="Logo Icon">
                         @else
-                            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
+                            <img src="{{ url('https://drive.google.com/uc?id=119tvwRPZ9mAC3qRfVy8-Sze1ixsdltNB') }}" alt="Logo Icon">
+                            {{-- <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon"> --}}
                         @endif
                     </div>
                     <div class="title">{{Voyager::setting('admin.title', 'VOYAGER')}}</div>
@@ -16,10 +18,18 @@
             </div><!-- .navbar-header -->
 
             <div class="panel widget center bgimage"
-                 style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;">
+                {{-- style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;"> --}}
+                
+                {{-- TEMP --}}
+                style="background-image:url('https://drive.google.com/uc?id=1UDvpqpCeuCimOPFlJSxuGM7PCY0Pme8l'); background-size: cover; background-position: 0px;">
+                
                 <div class="dimmer"></div>
                 <div class="panel-content">
-                    <img src="{{ $user_avatar }}" class="avatar" alt="{{ app('VoyagerAuth')->user()->name }} avatar">
+                    {{-- <img src="{{ $user_avatar }}" class="avatar" alt="{{ app('VoyagerAuth')->user()->name }} avatar"> --}}
+                    
+                    {{-- TEMP --}}
+                    <img src="{{ url('https://drive.google.com/uc?id=1Hl55A5yqqXSJnRyt_qQeYPemx34Lljf0') }}" class="avatar" alt="{{ app('VoyagerAuth')->user()->name }} avatar">
+                    
                     <h4>{{ ucwords(app('VoyagerAuth')->user()->name) }}</h4>
                     <p>{{ app('VoyagerAuth')->user()->email }}</p>
 
--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -12,9 +12,15 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css">
         <link rel="stylesheet" href="{{ voyager_asset('css/rtl.css') }}">
     @endif
+    {{-- {{url('https://drive.google.com/open?id=1UDvpqpCeuCimOPFlJSxuGM7PCY0Pme8l')}} --}}
+    {{-- {{die('dead')}} --}}
     <style>
         body {
-            background-image:url('{{ Voyager::image( Voyager::setting("admin.bg_image"), voyager_asset("images/bg.jpg") ) }}');
+            /* TEMP */
+            background-image:url('https://drive.google.com/uc?id=1UDvpqpCeuCimOPFlJSxuGM7PCY0Pme8l');
+            /* TEMP */
+
+            /* background-image:url('{{ Voyager::image( Voyager::setting("admin.bg_image"), voyager_asset("images/bg.jpg") ) }}'); */
             background-color: {{ Voyager::setting("admin.bg_color", "#FFFFFF" ) }};
         }
         body.login .login-sidebar {
@@ -49,9 +55,11 @@
                     <div class="logo-title-container">
                         <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
                         @if($admin_logo_img == '')
-                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
+                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ URL('https://drive.google.com/uc?id=119tvwRPZ9mAC3qRfVy8-Sze1ixsdltNB') }}" alt="Logo Icon">
+                        {{-- <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon"> --}}
                         @else
-                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
+                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ URL('https://drive.google.com/uc?id=119tvwRPZ9mAC3qRfVy8-Sze1ixsdltNB') }}" alt="Logo Icon">
+                        {{-- <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon"> --}}
                         @endif
                         <div class="copy animated fadeIn">
                             <h1>{{ Voyager::setting('admin.title', 'Voyager') }}</h1>
